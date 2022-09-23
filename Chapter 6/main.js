6facb467b5d280658e24cb54508439eef654fe72
Brandon Bautista <109395003+BranBautista@users.noreply.github.com>
2022-09-22 19:10:05 -0500

    Update main.js

--- Chapter 6/main.js
+++ Chapter 6/main.js
@@ -1,9 +1,12 @@
-//Va a ser una cadena. 
-//Tengo primero el objeto Shape (figura) y tiene 1 propiedad pEdges y 1 metodo fnDisplay
-//luego Un cuadrilatero es una figura entonces el objeto hereda la propiedad de Shape (pEdges y fnDisplay) y ademas 2 metodos fnArea, fnPerimeter
-//luego Un cuadrado es un cuadrilatero, y por lo tanto el objecto cuadradado hereda todas estas propiedades. 
-//triangulo es una figura entonces hereda la propiedad y el metodo de Shape y tiene los dos metodos fnArea, fnPerimeter
+// *************************************** //
+// *****  JavaScript Problem Set #0  ***** //
+// *************************************** //
 
+// ----------------------------- //
+// ******-- Chapter 06 --****** //
+// ----------------------------- //
+
+// ******-- Exercise 01 --****** //
 
 class Shape{
     constructor (pEdges){
