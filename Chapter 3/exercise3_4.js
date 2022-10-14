79d689dbc45ff5516b791416d5fc0baa70865202
Brandon Bautista <109395003+BranBautista@users.noreply.github.com>
2022-10-14 18:58:07 -0500

    Create exercise3_4.js

---
+++ Chapter 3/exercise3_4.js
@@ -0,0 +1,57 @@
+let data = new Array(1600);
+
+function dataFulfilled (array){
+    for (let i = 0; i < array.length; i++){
+        if(i < array.length/4){
+            array[i] = 'yellow';
+        }
+        else if (i < array.length/2){
+            array[i] = 'green';
+        }
+        else if (i < (3*array.length)/4){
+            array[i] = 'blue';
+        }
+        else {
+            array[i] = 'red';
+        }
+    }
+    return array;
+}
+
+dataFulfilled(data);
+
+let sizeX = Math.sqrt(data.length);
+let sizeY = Math.sqrt(data.length);
+
+//This is going to be our creator function:
+function Image(data, sizeX, sizeY, name) {
+    if (sizeX > 0 && sizeY > 0) { //Here I prevent negative values
+        this.data = data;
+        this.width = sizeX;
+        this.heigth = sizeY;
+        this.name = name;
+    }
+    else {
+        return console.error('Invalid size');
+    }
+}
+
+Image.prototype.getPixel = function (positionX, positionY) {
+    //Here I prevent offsize pixels and negative values  
+    if ((positionX <= this.width && positionX > 0) && (positionY <= this.heigth && positionY > 0)) {
+        //I supposed that 40 items in the array form a raw of pixels of the 2d image
+        //so every 40 itmes change to a lower raw, and so on
+        let positionArray = (positionY-1)*this.width + positionX;
+        return this.data[positionArray-1]
+    } else {
+        return 'This pixel is not part of the image';
+    }
+}
+
+let img = new Image(data, sizeX, sizeY, 'myImage');
+
+//console.log(img.data);
+console.log(img.width);
+console.log(img.heigth);
+console.log(img.name);
+console.log(img.getPixel(18, 4));
