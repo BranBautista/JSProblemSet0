2313e7427e95022e7a014c85fac8f28998741c9c
Brandon Bautista <109395003+BranBautista@users.noreply.github.com>
2022-10-14 18:56:32 -0500

    Create exercise3_2.js

---
+++ Chapter 3/exercise3_2.js
@@ -0,0 +1,9 @@
+function add() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum = sum + arguments[i];
+    }
+    return sum;
+}
+
+console.log(add(1, 2) + add(1, 4, 6, 7, 2));
