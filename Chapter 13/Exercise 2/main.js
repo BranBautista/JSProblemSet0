d8b6b609bcbbfc456be8d9c2326603af783625c3
Brandon Bautista <109395003+BranBautista@users.noreply.github.com>
2022-10-14 19:18:50 -0500

    Update main.js

--- Chapter 13/Exercise 2/main.js
+++ Chapter 13/Exercise 2/main.js
@@ -1,9 +1,16 @@
-var counter = 0;
+const btn = document.getElementById("btnThreeTimes");
+const btnUsage = document.getElementById("buttonUsage");
 
-$('button').click(function () {
-    counter = counter + 1;
-    $('#buttonUsage').val(counter)
-    if (counter == 3){
-        $('button').prop('disabled',true);
-    }
-});
+(function () {
+
+    let counter = 0;
+
+    btn.addEventListener("click", function () {
+        counter = counter + 1;
+        btnUsage.value = counter;
+        if (counter == 3) {
+            btn.disabled = true;
+        }
+    })
+
+}())
