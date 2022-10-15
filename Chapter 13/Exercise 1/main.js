fc7fea0fc0be53841b27a43f27785356589547b1
Brandon Bautista <109395003+BranBautista@users.noreply.github.com>
2022-10-14 19:17:25 -0500

    Update main.js

--- Chapter 13/Exercise 1/main.js
+++ Chapter 13/Exercise 1/main.js
@@ -1,13 +1,24 @@
-$('document').ready(function () {
-    var table = $('<div>').addClass('gridcontainer');
-    for (i = 0; i < 25; i++) {
-        var row = $('<div>').addClass('element').text(i + 1);
-        table.append(row);
-    }
 
-    $('#table').append(table);
 
-    $('.element').click(function () {
-        alert($(this).text());
-    });
-});
+const tableContainer = document.getElementById("table");
+
+const table = document.createElement("div");
+table.classList.add("gridcontainer");
+
+for (i=0; i<25; i++){
+    const row = document.createElement("div");
+    row.classList.add("element");
+    const text = document.createTextNode(`${i+1}`);
+    row.appendChild(text);
+    table.appendChild(row);
+}
+
+tableContainer.appendChild(table);
+
+const elements = document.getElementsByClassName("element");
+
+for (let i = 0; i < elements.length; i++){
+    elements[`${i}`].addEventListener("click", function (){
+        alert(this.innerHTML);
+    })
+}
