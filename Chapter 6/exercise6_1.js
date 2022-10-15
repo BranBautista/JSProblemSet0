84dac429f6f66c674810359660252830075d6af6
Brandon Bautista <109395003+BranBautista@users.noreply.github.com>
2022-10-14 19:09:55 -0500

    Update exercise6_1.js

--- Chapter 6/exercise6_1.js
+++ Chapter 6/exercise6_1.js
@@ -1,3 +1,57 @@
+class Shape{
+    constructor (pEdges){
+        this.pEdges = pEdges;
+    }
+    fnDisplay (){
+        return `I have ${this.pEdges} number of Edges`;
+    }
+}
+
+class Quadrilateral extends Shape{
+    constructor (pEdges){
+        super(pEdges)
+    }
+    fnPerimeter (perimeter){
+        return `The length of my perimeter is ${perimeter}`;
+    }
+    fnArea (area){
+        return `My area is ${area}`;
+    }
+}
+
+
+class Triangle extends Shape{
+    constructor (pEdges){
+        super(pEdges)
+    }
+    fnPerimeter (perimeter){
+        return `The length of my perimeter is ${perimeter}`;
+    }
+    fnArea (area){
+        return `My area is ${area}`;
+    }
+}
+
+
+class Square extends Quadrilateral{
+    constructor (pEdges){
+        super(pEdges)
+    }
+}
+
+let shape = new Shape();
+let quadrilateral = new Quadrilateral(4);
+let triangle = new Triangle(3);
+let square = new Square(4);
+
+console.log(quadrilateral.fnDisplay('quadrilateral'))
+console.log(quadrilateral.fnPerimeter('30cm'))
+console.log(quadrilateral.fnArea('15cm2'))
+
+
+// SECOND EXAMPLE
+
+
 class Physics{
     constructor (mathematics1, mathematics2, mathematics3, mathematics4, mathematics5, mathematics6){
         this.mathematics1 = mathematics1;
